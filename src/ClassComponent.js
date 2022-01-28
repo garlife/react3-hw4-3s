f3ccb875aadf7e6bcdb94c2964a421fea7a02fab
garlife <garlife@gmail.com>
2022-01-28 21:34:34 +0300

    Class component added

--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -1,26 +1,44 @@
 import React from 'react';
 
 export default class ClassComponent extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       counter: 0,
-      comment: []
-    }
-    this.handlerGoldDigger = this.handlerGoldDigger.bind(this)
-    this.handlerComment = this.handlerComment.bind(this)
+      comment: [],
+    };
+    this.handlerGoldDigger = this.handlerGoldDigger.bind(this);
+    this.handlerComment = this.handlerComment.bind(this);
 
-    this.myRef = React.createRef()
-
+    this.myRef = React.createRef();
   }
 
-  handlerGoldDigger = () => this.setState( (prevState) => ({counter: prevState.counter + 1 }));
+  handlerGoldDigger = () =>
+    this.setState((prevState) => ({ counter: prevState.counter + 1 }));
 
   handlerComment = () => {
-    this.commentValue = myRef.current.value;
-    this.comments = [...this.state.comment, this.commentValue]
-  }
+    //присваиваем переменой значение dom узла по ссылке реф
+    let commentValue = this.myRef.current.value; 
+    //присваемаем массив с первоначальным стэйтом другой переменной
+    let commentMassive = this.state.comment; 
+    //пушим вэлью рэфа в массив
+    commentMassive.push(commentValue);
+    console.log(commentMassive)
+    this.setState({
+      'comment': commentMassive
+    })
+    this.myRef.current.value = null;
+  };
 
+  //упрощенная версия без доп присвиваний переменным
+  // handlerComment = () => {
+  //   this.state.comment.push(this.myRef.current.value);
+  //   console.log(this.state.comment)
+  //   this.setState({
+  //     'comment': this.state.comment
+  //   })
+  //   this.myRef.current.value = null;
+  // };
 
 
   render() {
@@ -28,12 +46,23 @@ export default class ClassComponent extends React.Component {
       <>
         <button onClick={this.handlerGoldDigger}>Позолоти ручки!</button>
         <div>Подарено {this.state.counter} золотых</div>
-        <h3>{this.state.counter % 2 === 0 ? 'чёт требует добавки, жми кнопку!' : 'нечёт к несчастью, добавь!'}</h3>
+        <h3>
+          {this.state.counter % 2 === 0
+            ? 'чёт требует добавки, жми кнопку!'
+            : 'нечёт к несчастью, добавь!'}
+        </h3>
         <div>
           <textarea ref={this.myRef}></textarea>
           <br />
           <button onClick={this.handlerComment}>Оставить отзыв</button>
         </div>
+        <div>
+          <ul>
+            {this.state.comment.map((item, index) => 
+              <li key={index.toString()}>{item}</li>)}
+          </ul>
+        </div>
+        {this.commentValue}
       </>
     );
   }
